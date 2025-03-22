0bf80f6d0ca5f41e1c06b95169f16b712d686705
owlushorler <owlushorler@gmail.com>
2025-03-22 08:40:19 +0100

    nice

--- app/component/secondCentent/footer.js
+++ app/component/secondCentent/footer.js
@@ -63,11 +63,11 @@ const Footer = () => {
 
               <a
                 className="text-white transition hover:text-gray-700/75"
-                href="#"
+                href="https://www.youtube.com/@TopKenRoofingSolutions" // Replace with your YouTube channel link
                 target="_blank"
                 rel="noreferrer"
               >
-                <span className="sr-only"> Twitter </span>
+                <span className="sr-only">YouTube</span>
 
                 <svg
                   className="size-6"
@@ -75,8 +75,21 @@ const Footer = () => {
                   viewBox="0 0 24 24"
                   aria-hidden="true"
                 >
-                  <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                  <path d="M23.496 6.168a3.002 3.002 0 00-2.115-2.115C19.646 4.16 12 4.16 12 4.16s-7.646 0-9.381.893A3.002 3.002 0 00.504 6.168C.002 8.1 0 12 0 12s0 3.9.504 5.832a3.002 3.002 0 002.115 2.115C4.354 19.84 12 19.84 12 19.84s7.646 0 9.381-.893a3.002 3.002 0 002.115-2.115C24 15.9 24 12 24 12s0-3.9-.504-5.832zm-13.496 8.174V9.658l6.337 2.342-6.337 2.342z" />
                 </svg>
+              </a>
+
+              <a
+                className="text-white transition hover:text-gray-700/75"
+                href="https://www.tiktok.com/@topkenroofingsolutions?_t=ZM-8uhXHrkLW90&_r=1"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span className="sr-only">TikTok</span>
+
+                <div className="flex items-center justify-center w-8 h-8 bg-blue-950 rounded-full">
+                  <span className="text-sm  text-white">Tik</span>
+                </div>
               </a>
 
               <a
